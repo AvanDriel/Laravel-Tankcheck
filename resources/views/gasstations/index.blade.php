104642d3316dc9584b38b5e1bb8ec21904a5269d
avandriel <avandriel1998@gmail.com>
2017-10-11 15:14:29 +0200

    Added Location Search

--- resources/views/gasstations/index.blade.php
+++ resources/views/gasstations/index.blade.php
@@ -6,15 +6,57 @@
 	<div id="map"></div>
 
 	<script>
-		
-	function initMap() {
-      var uluru = {lat: 51.933303, lng: 4.45201};
-          var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 13,
-            center: uluru
-          });
 
-    	@foreach ($gasstations as $gasstation)
+        function initMap() {
+            var uluru = {lat: 51.933303, lng: 4.45201};
+            
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 13,
+                center: uluru,
+                mapTypeId: 'roadmap'
+            });
+
+            var defaultBounds = new google.maps.LatLngBounds(
+              new google.maps.LatLng(-33.8902, 151.1759),
+              new google.maps.LatLng(-33.8474, 151.2631));
+
+            var input = document.getElementById('searchfield');
+
+            var searchBox = new google.maps.places.SearchBox(input, {
+              bounds: defaultBounds
+            });
+
+
+            // Listen for the event fired when the user selects a prediction and retrieve
+            // more details for that place.
+            searchBox.addListener('places_changed', function() {
+              var places = searchBox.getPlaces();
+
+              if (places.length == 0) {
+                return;
+              }
+
+            // For each place, get the icon, name and location.
+              var bounds = new google.maps.LatLngBounds();
+              places.forEach(function(place) {
+                if (!place.geometry) {
+                  console.log("Returned place contains no geometry");
+                  return;
+                }
+
+                if (place.geometry.viewport) {
+                  // Only geocodes have viewport.
+                  bounds.union(place.geometry.viewport);
+                } else {
+                  bounds.extend(place.geometry.location);
+                }
+              });
+              map.fitBounds(bounds);
+            });
+
+        
+
+        @foreach ($gasstations as $gasstation)
             var location{{$gasstation->id}}= {lat: {{$gasstation->latitude}}, lng: {{$gasstation->longitude}}};
 
             var contentString{{$gasstation->id}} = '<div id="content">'+
@@ -39,12 +81,14 @@
             marker{{$gasstation->id}}.addListener('click', function() {
                 infowindow{{$gasstation->id}}.open(map, marker{{$gasstation->id}});
               });
-       	@endforeach
+        @endforeach 
     }
+
 	</script>
-		
+
 	<script
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7jz7tGATiylYJH4Qa--VfUWz0GWfOvNc&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7jz7tGATiylYJH4Qa--VfUWz0GWfOvNc&&libraries=places&callback=initMap"
+         async defer">
     </script>
 
 @endsection
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,25 @@
 <div class="footer">
-	<h3>Footer</h3>
+	
+	{{-- <div class="search-form container">
+		<form class="form-inline" method="POST" action="">
+			
+			{{csrf_field()}}
+
+			<div class="form-group"> --}}
+				
+				<input type="text" class="form-control" id="searchfield" name="location" placeholder="Search for a location" required>
+			
+		{{-- 	</div>
+
+			<div class="form-group">
+
+				<button type="submit" class="btn btn-primary">Search</button>
+
+			</div>
+
+			@include ('layouts.errors')
+
+		</form>
+	</div>	 --}}	
+
 </div>
